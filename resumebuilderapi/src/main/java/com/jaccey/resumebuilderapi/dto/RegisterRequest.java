932df3554d1af332ebd8026a92dc8256e5ac4e9c
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2025-11-11 23:29:55 +0100

    User object

---
+++ resumebuilderapi/src/main/java/com/jaccey/resumebuilderapi/dto/RegisterRequest.java
@@ -0,0 +1,28 @@
+package com.jaccey.resumebuilderapi.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegisterRequest {
+    @NotBlank(message = "Name is required.")
+    @Size(min = 2, max = 20, message = "Name must be between 2 and 20 characters.")
+    private String name;
+
+    @Email(message = "Email should be valid.")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required.")
+    @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters.")
+    private String password;
+    private String profileImageUrl;
+}
